ded11ec345097a4cc58320638d8d4ca8efed659d
Mikel Rojas <mikelarr.2004@gmail.com>
2024-08-03 09:35:43 -0600

    work in styles

--- src/common/NavBar.tsx
+++ src/common/NavBar.tsx
@@ -4,18 +4,18 @@ import React from "react";
 export const NavBar: React.FC<{}> = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" style={{ marginLeft: '12px' }} href="/">NASA</a>
+      <a className="navbar-brand fs-2 fw-bold" style={{ marginLeft: '12px' }} href="/">NASA</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
       </button>
 
       <div className="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
-        <ul className="navbar-nav">
+        <ul className="navbar-nav fs-3 fw-bold">
           <li className="nav-item active">
             <a className="nav-link" href="/">Home</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/picture">Astronomy Picture</a>
+            <a className="nav-link" href="/picture">Astronomy-Picture</a>
           </li>
           <li className="nav-item">
             <a className="nav-link" href="#">Events</a>
