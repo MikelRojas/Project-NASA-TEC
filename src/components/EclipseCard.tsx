61a4796a56eed07a3916c007cb876efca62ddb84
Mikel Rojas <mikelarr.2004@gmail.com>
2024-08-08 20:58:53 -0600

    Add functios to page Events

--- src/components/EclipseCard.tsx
+++ src/components/EclipseCard.tsx
@@ -8,42 +8,77 @@ interface EclipseData {
   places: string;
 }
 
-export const EclipseCard: React.FC = () => {
+interface EclipseCardProps {
+  startDate: string;
+  endDate: string;
+  type: 'solar' | 'lunar';
+}
+
+const EclipseImg = (type: string, mode: string): string => {
+  switch (mode) {
+    case 'Total':
+      return type === 'solar' ? '/images/solar-total.jpg' : '/images/lunar-total.png';
+    case 'Partial':
+      return type === 'solar' ? '/images/solar-partial.jpg' : '/images/lunar-partial.png';
+    case 'Annular':
+      return '/images/solar-anular.jpg';
+    case 'Penumbral':
+      return '/images/lunar-penumbral.png';
+    default:
+      return '/images/default.jpg';
+  }
+}
+
+export const EclipseCard: React.FC<EclipseCardProps> = ({ startDate, endDate, type }) => {
   const [eclipseData, setEclipseData] = useState<EclipseData[] | null>(null);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    const request = 'http://127.0.0.1:8000/eclipses?start_date=2024-01-01&end_date=2024-12-01&type=solar';
+    const request = `http://127.0.0.1:8000/eclipses?start_date=${startDate}&end_date=${endDate}&type=${type}`;
     
     fetch(request)
       .then(response => {
+        if (response.status === 404) {
+          throw new Error('There are no events on these dates.');
+        }
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Error in request');
         }
         return response.json();
       })
       .then(result => {
-        setEclipseData(result);
+        if (result.length === 0) {
+          setError('There are no events on these dates.');
+          setEclipseData(null);
+        } else {
+          setEclipseData(result);
+          setError(null);
+        }
       })
       .catch(error => {
-        setError(`Bad request: ${error.message}`);
+        setError(`Error in request: ${error.message}`);
+        setEclipseData(null);
       });
-  }, []);
+  }, [startDate, endDate, type]);
 
   return (
-    <>
+    <div className="container mt-4">
       {error ? (
-        <p>{error}</p>
+        <p className="text-danger">{error}</p>
       ) : (
-        <div>
+        <div className="row">
           {eclipseData ? (
             eclipseData.map((eclipse, index) => (
-              <div key={index}>
-                <p>Type: {eclipse.type}</p>
-                <p>Date: {eclipse.date}</p>
-                <p>Hour: {eclipse.hour}</p>
-                <p>Mode: {eclipse.mode}</p>
-                <p>Places: {eclipse.places}</p>
+              <div className="col-md-3 mb-3" key={index}>
+                <div className="card eclipse-card text-center">
+                  <img src={EclipseImg(eclipse.type, eclipse.mode)} className="card-img-top card-image" alt={eclipse.mode} />
+                  <div className="card-body">
+                    <h5 className="card-title">{eclipse.mode} Eclipse</h5>
+                    <p className="card-text">Date: {eclipse.date}</p>
+                    <p className="card-text">Maximum Point: {eclipse.hour}</p>
+                    <p className="card-text">Places: {eclipse.places}</p>
+                  </div>
+                </div>
               </div>
             ))
           ) : (
@@ -51,6 +86,6 @@ export const EclipseCard: React.FC = () => {
           )}
         </div>
       )}
-    </>
+    </div>
   );
 };
--- src/pages/Events/index.tsx
+++ src/pages/Events/index.tsx
@@ -1,13 +1,107 @@
-import { EclipseCard } from '../../components/EclipseCard'
-import './styles.css'
+import { useEffect, useState } from 'react';
+import { EclipseCard } from '../../components/EclipseCard';
+import './styles.css';
 
-export const Events: React.FC<{}> = () => {
-  return(
+export const Events: React.FC = () => {
+  const [startDate, setStartDate] = useState<Date>(new Date('2024-08-01'));
+  const [endDate, setEndDate] = useState<Date>(new Date('2026-08-07'));
+  const [selectedEvent, setSelectedEvent] = useState<string>('Asteroids and Comets');
+  const [eclipseType,setEclipseType] = useState<'solar'|'lunar'>('solar');
+
+  const handleEventSelection = (event: string) => {
+    setSelectedEvent(event);
+    if(event==='Solar Eclipses'){
+      setEclipseType('solar');
+    }
+    else if(event==='Lunar Eclipses'){
+      setEclipseType('lunar');
+    }
+  };
+
+  const handleStartDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newStartDate = new Date(e.target.value);
+    if(newStartDate<minDate){
+      alert('End date cannot be earlier than 01-01-1901.');
+    }else if(newStartDate>endDate){
+      alert('Start date cannot be later than end date.');
+    } else{
+      setStartDate(newStartDate);
+    }
+  };
+
+  const handleEndDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newEndDate = new Date(e.target.value);
+    if(newEndDate>maxDate){
+      alert('End date cannot be later than 01-12-2070.');
+    }else if (newEndDate<startDate){
+      alert('End date cannot be earlier than start date.');
+    }else{
+      setEndDate(newEndDate);
+    }
+  };
+
+  const minDate = new Date("1901-01-01");
+  const maxDate = new Date("2070-12-01");
+  return (
     <div className="bg-image img-fluid">
-        <div style={{ position: 'relative',minHeight: '100vh'}}>
-          <EclipseCard></EclipseCard>
+      <div style={{ position: 'relative', minHeight: '100vh' }}>
+        <h1>Upcoming Events</h1>
+        <div className="date-inputs">
+          <label>
+            Start Date:
+            <input 
+            type="date" 
+            value={startDate.toISOString().split('T')[0]} 
+            onChange={handleStartDateChange}
+            min = {minDate.toISOString().split('T')[0]}
+            max = {maxDate.toISOString().split('T')[0]}
+             />
+          </label>
+          <label>
+            End Date:
+            <input 
+            type="date" 
+            value={endDate.toISOString().split('T')[0]} 
+            onChange={handleEndDateChange} 
+            min = {minDate.toISOString().split('T')[0]}
+            max = {maxDate.toISOString().split('T')[0]}
+            />
+          </label>
         </div>
+        <h2>Select Event:</h2>
+        <div className="event-buttons">
+          <button
+            className={`event-button ${selectedEvent === 'Asteroids and Comets' ? 'active' : ''}`}
+            onClick={() => handleEventSelection('Asteroids and Comets')}
+          >
+            Asteroids and Comets
+          </button>
+          <button
+            className={`event-button ${selectedEvent === 'Solar Eclipses' ? 'active' : ''}`}
+            onClick={() => handleEventSelection('Solar Eclipses')}
+          >
+            Solar Eclipses
+          </button>
+          <button
+            className={`event-button ${selectedEvent === 'Lunar Eclipses' ? 'active' : ''}`}
+            onClick={() => handleEventSelection('Lunar Eclipses')}
+          >
+            Lunar Eclipses
+          </button>
+        </div>
+        <div className='result-container'>
+        {(selectedEvent === 'Solar Eclipses' || selectedEvent === 'Lunar Eclipses') && (
+           <EclipseCard
+           startDate={startDate.toISOString().split('T')[0]}
+           endDate={endDate.toISOString().split('T')[0]}
+           type={eclipseType}
+         />
+          )}
+        {selectedEvent === 'Asteroids and Comets' && (
+            <div>No data available for Asteroids and Comets.</div>
+          )}
+        </div>
+      </div>
     </div>
-
-  )
+  );
 }
