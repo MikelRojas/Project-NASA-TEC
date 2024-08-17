221b09a1beb88f3b84a35f6386f9502fe95884b8
Mikel Rojas <mikelarr.2004@gmail.com>
2024-08-17 16:21:26 -0600

    Start to worck in login and my events

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD29Q3ZflABW_k6Qk9k_DzgBKvTB4wMlA0",
+    authDomain: "dbastronomy-321c6.firebaseapp.com",
+    projectId: "dbastronomy-321c6",
+    storageBucket: "dbastronomy-321c6.appspot.com",
+    messagingSenderId: "1047530071409",
+    appId: "1:1047530071409:web:56218f48c0b18d84aec2d0",
+    measurementId: "G-8HKB29W77S"
+  };
+  
+  // Initialize Firebase
+  const appFirebase = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(appFirebase);
 
 
 export const Login: React.FC<{}> = () =>{
     return(
-        <>
-        </>
+        <div>
+
+        </div>
     )
 }
